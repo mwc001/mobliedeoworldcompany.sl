65fc1f26c24cf20a14a04a2605c52016f47db655
wdd130 <wdd13000@gmail.com>
2026-07-12 16:51:54 +0000

    Run migrations on Render startup

--- start.sh
+++ start.sh
@@ -6,5 +6,6 @@ PORT="${PORT:-8000}"
 python -m pip install --disable-pip-version-check -q -r requirements/production.txt || true
 python -m pip install --disable-pip-version-check -q gunicorn==20.1.0 || true
 python manage.py collectstatic --noinput || true
+python manage.py migrate --noinput || true
 
 exec python -m gunicorn --bind 0.0.0.0:${PORT} SMS.wsgi:application
